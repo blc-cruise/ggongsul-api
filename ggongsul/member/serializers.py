d78fa2fe647fc90e1f4510c25ba8e616dc788b0e
zezaeoh <zezaeoh@gmail.com>
2021-02-20 15:35:38 +0900

    Add profile image for members

--- ggongsul/member/serializers.py
+++ ggongsul/member/serializers.py
@@ -1,6 +1,9 @@
 import logging
 import secrets
 
+from typing import Optional
+
+from django.conf import settings
 from django.db import transaction
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -10,7 +13,12 @@ from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from rest_framework.validators import UniqueValidator
 
 from ggongsul.core.generics import APISerializer
-from ggongsul.member.models import SocialAccount, MemberDetail, Member
+from ggongsul.member.models import (
+    SocialAccount,
+    MemberDetail,
+    Member,
+    MemberProfileImage,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -55,6 +63,9 @@ class SignupSerializer(APISerializer):
         max_length=150, validators=[UniqueValidator(queryset=Member.objects.all())]
     )
     channel_in = serializers.ChoiceField(choices=MemberDetail.ChannelIn.choices)
+    profile_image = serializers.PrimaryKeyRelatedField(
+        queryset=MemberProfileImage.objects.all(), allow_null=True
+    )
 
     def validate_policy_agree_yn(self, value: bool):
         if not value:
@@ -77,10 +88,18 @@ class SignupSerializer(APISerializer):
         if not is_valid:
             raise ValidationError(msg)
 
+        profile_image: Optional[MemberProfileImage] = attrs.get("profile_image", None)
+        if profile_image and profile_image.member_id:
+            raise ValidationError(_("이미 다른 멤버가 사용중인 프로필 사진입니다."))
+
         if SocialAccount.objects.filter(provider=signup_type, uid=uid).exists():
 
             # 해당 소셜 계정이 active하면
-            if SocialAccount.objects.filter(provider=signup_type, uid=uid).last().member.is_active:
+            if (
+                SocialAccount.objects.filter(provider=signup_type, uid=uid)
+                .last()
+                .member.is_active
+            ):
                 raise ValidationError(_("해당 소셜 계정으로 가입한 멤버가 존재합니다."))
 
             # 해당 소셜 계정이 deactive하면 기존 정보를 update시킨다.
@@ -100,6 +119,7 @@ class SignupSerializer(APISerializer):
             uid=uid,
             provider=signup_type,
             adv_agree_yn=attrs["adv_agree_yn"],
+            profile_image=profile_image,
         )
         return m.process_login()
 
@@ -111,6 +131,7 @@ class SignupSerializer(APISerializer):
         provider: SocialAccount.Provider,
         uid: str,
         adv_agree_yn: bool,
+        profile_image: Optional[MemberProfileImage] = None,
     ):
         # create member
         m: Member = Member.objects.create_user(
@@ -129,12 +150,17 @@ class SignupSerializer(APISerializer):
             m.agreement.adv_agreed_yn = adv_agree_yn
             m.agreement.adv_agreed_at = cur_datetime
 
+        if profile_image:
+            profile_image.member_id = m.id
+            profile_image.save()
+
         # save related objects
         m.detail.save()
         m.agreement.save()
         m.save()
         return m
 
+    @transaction.atomic
     def _update_social_account_member(
         self,
         username: str,
@@ -142,8 +168,8 @@ class SignupSerializer(APISerializer):
         provider: SocialAccount.Provider,
         uid: str,
         adv_agree_yn: bool,
+        profile_image: Optional[MemberProfileImage] = None,
     ):
-
         m = SocialAccount.objects.filter(provider=provider, uid=uid).last().member
         cur_datetime = timezone.now()
 
@@ -159,6 +185,15 @@ class SignupSerializer(APISerializer):
         if adv_agree_yn:
             m.agreement.adv_agreed_yn = adv_agree_yn
             m.agreement.adv_agreed_at = cur_datetime
+
+        if profile_image:
+            # 이미 profile image 가 존재하는 경우
+            if hasattr(m, "profile_image"):
+                m.profile_image.member_id = None
+                m.profile_image.save()
+
+            profile_image.member_id = m.id
+            profile_image.save()
 
         # save related objects
         m.detail.save()
@@ -178,6 +213,7 @@ class MemberSerializer(serializers.ModelSerializer):
     next_membership_payment = serializers.SerializerMethodField()
     start_subscription_date = serializers.SerializerMethodField()
     end_subscription_date = serializers.SerializerMethodField()
+    profile_image = serializers.SerializerMethodField()
 
     def get_next_membership_payment(self, obj: Member):
         np = obj.next_membership_payment()
@@ -197,11 +233,17 @@ class MemberSerializer(serializers.ModelSerializer):
             return None
         return sub.ended_at.strftime("%Y년 %m월 %d일")
 
+    def get_profile_image(self, obj: Member):
+        if hasattr(obj, "profile_image"):
+            return obj.profile_image.image.url
+        return settings.DEFAULT_PROFILE_IMAGE_URL
+
     class Meta:
         model = Member
         fields = [
             "id",
             "username",
+            "profile_image",
             "has_membership_benefits",
             "is_membership_activated",
             "total_membership_days",
@@ -210,3 +252,33 @@ class MemberSerializer(serializers.ModelSerializer):
             "start_subscription_date",
             "end_subscription_date",
         ]
+
+
+class MemberProfileImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MemberProfileImage
+        fields = [
+            "id",
+            "image",
+        ]
+
+
+class UpdateProfileImageSerializer(APISerializer):
+    profile_image = serializers.PrimaryKeyRelatedField(
+        queryset=MemberProfileImage.objects.all()
+    )
+
+    def validate(self, attrs: dict):
+        profile_image: MemberProfileImage = attrs["profile_image"]
+        if profile_image.member_id:
+            raise ValidationError(_("이미 다른 멤버가 사용중인 프로필 사진입니다."))
+
+        member: Member = self.context["request"].user
+        if hasattr(member, "profile_image"):
+            member.profile_image.member_id = None
+            member.profile_image.save()
+
+        profile_image.member_id = member.id
+        profile_image.save()
+
+        return {"profile_image": profile_image.image.url}
